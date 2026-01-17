69ae66d4d3bf88b933744980490ce90a1ea6bb25
semevi <140612005+semevi@users.noreply.github.com>
2026-01-17 14:12:26 +0000

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import { Pool } from 'pg';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 30000;
 
 app.use(morgan('dev'));
 app.use(cors());
